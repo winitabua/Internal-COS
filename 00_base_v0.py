29536fa3bed5c7f99f9bf73f42235a80c2d1e705
winitabua <134983147+winitabua@users.noreply.github.com>
2023-06-27 12:57:01 +1200

    00_base_v0

---
+++ 00_base_v0.py
@@ -0,0 +1,17 @@
+"""
+base component for my area and perimeter calculation program
+
+v0 - skeleton setup
+v1 - user yes no checker and instructions
+v2 - shape + type of calculation selection, calculation of area / perimeter and create a loop
+v3 - display the history
+
+by Winita Buaban
+"""
+
+
+
+
+
+
+
