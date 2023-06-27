337b5300c7fbc8e985c498cacea3e80aafd0340d
winitabua <134983147+winitabua@users.noreply.github.com>
2023-06-27 12:57:57 +1200

    00_base _v1

---
+++ 00_base _v1.py
@@ -0,0 +1,30 @@
+
+# All functions below this line
+
+# Instruction in a function, basic explanation
+def instructions():
+
+
+# Shape selection in function
+def shape_select():
+
+# function of area and perimeter calculation
+def func_rectangle():
+
+def func_circle():
+
+def func_triangle():
+
+def func_parallelogram():
+
+
+# loop, ask user whether they have more calculation to do
+
+
+# Display the history
+
+
+# main routine goes below here...
+print("Welcome to...")
+
+
