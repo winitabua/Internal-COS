cccf83be01ca8cf6b7603263aad17a3bc89506d4
winitabua <134983147+winitabua@users.noreply.github.com>
2023-06-27 13:00:01 +1200

    02_Instructions_v1

---
+++ 02_Instructions_v1.py
@@ -0,0 +1,15 @@
+
+# Instruction in a function, basic explanation
+def instructions():
+    print()
+    print("***** How to use area and perimeter calculation tool *****")
+    print("This is a program for students to check their maths homework "
+          "specifically about Area and Perimeter.\n"
+          "To use, please follow through each step; select shape & select type of calculation\n"
+          "Then program will display the answer.\n"
+          "You can do as many calculation as you want until you select finished.\n"
+          "The program will display your calculation history at the end of the session.")
+    print("Let's begin!")
+
+
+
