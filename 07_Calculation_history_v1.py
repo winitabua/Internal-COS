48e051e88e7aed60d3adbf0857ff7c0c1fcfb787
winitabua <134983147+winitabua@users.noreply.github.com>
2023-07-27 22:18:08 +1200

    07_Calculation_history_v1

---
+++ 07_Calculation_history_v1.py
@@ -0,0 +1,6 @@
+
+# display history
+print("\nCalculation History:")
+        for i, (area, perimeter) in enumerate(calculation_history, start=1):
+            print("Calculation {} - Area: {}, Perimeter: {}".format(i, area, perimeter))
+
